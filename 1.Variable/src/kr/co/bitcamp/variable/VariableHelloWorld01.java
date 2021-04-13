d6ff75b650715206fd3dab6db5a2d2ce9221d3d5
hong <kamill09@naver.com>
2021-04-13 19:39:10 +0900

    1.variable
    변수와 변수 선언

---
+++ 1.Variable/src/kr/co/bitcamp/variable/VariableHelloWorld01.java
@@ -0,0 +1,21 @@
+package kr.co.bitcamp.variable;
+/*
+ * 접근제한자 클래스선언 클래스이름 {
+ *     접근제한자 static 반환타입 메서드이름(문자열 배열 변수명) {
+ *        // 구현할 코드 작성
+ *     }  
+ *  }    
+ */
+
+/*
+ * 1. 접근제한자: public, private, protected, (default)
+ * 2. 클래스선언: class -> 객체를 생성하는 틀, 프레임, 공장, 템플릿
+ * 3. 클래스이름: 카멜케이스(단어와 단어사이의 구분을 대문자로 함)
+ * 4. 메서드이름: 함수 -> 어떤 특정한 동작이나 작업, 행위 등을 수행.             
+ */
+
+public class VariableHelloWorld01 {
+    public static void main(String[] args) {
+        
+    }
+}
