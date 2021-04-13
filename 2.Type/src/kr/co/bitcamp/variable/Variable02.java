6a3c786b363f973fc7e6c9dd8f0ad6fd1e610b00
hong <kamill09@naver.com>
2021-04-13 19:42:02 +0900

    2. Type
    자료형

---
+++ 2.Type/src/kr/co/bitcamp/variable/Variable02.java
@@ -0,0 +1,27 @@
+package kr.co.bitcamp.variable;
+
+public class Variable02 {
+    public static void main(String[] args) {
+        // 단축키 main+ctrl+space
+        // 단축키 sysout+ctrl+space
+        System.out.println(Byte.BYTES); // 바이트 계산
+        System.out.println(Byte.SIZE); // 비트 계산
+        System.out.println(Short.BYTES);
+        System.out.println(Short.SIZE);
+        System.out.println();
+        
+        // byte, short, int, long, char
+        System.out.println("byte:\t"+Byte.BYTES+"(바이트)"+"->"+Byte.SIZE+
+                "(비트)\t"+Byte.MAX_VALUE+"~"+Byte.MIN_VALUE);
+        System.out.println("short:\t"+Short.BYTES+"(바이트)"+"->"+Short.SIZE+
+                "(비트)\t"+Short.MAX_VALUE+"~"+Short.MIN_VALUE);
+        System.out.println("int:\t"+Integer.BYTES+"(바이트)"+"->"+Integer.SIZE+
+                "(비트)\t"+Integer.MAX_VALUE+"~"+Integer.MIN_VALUE);
+        System.out.println("long:\t"+Long.BYTES+"(바이트)"+"->"+Long.SIZE+
+                "(비트)\t"+Long.MAX_VALUE+"~"+Long.MIN_VALUE);
+        System.out.println("char:\t"+Character.BYTES+"(바이트)"+"->"+Character.SIZE+
+                "(비트)\t"+(int)Character.MAX_VALUE+"~"+(int)Character.MIN_VALUE);
+
+    }
+
+}
